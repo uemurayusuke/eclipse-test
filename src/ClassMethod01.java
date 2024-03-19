a80480b65a1588283b60ce40960703bfc406357a
YusukeUemura <145743053+uemurayusuke@users.noreply.github.com>
2024-03-19 11:34:00 +0900

    クラス復習

--- src/ClassMethod01.java
+++ src/ClassMethod01.java
@@ -1,17 +1,11 @@
 
-public class ClassMethod01 {
 
+public class ClassMethod01 {
 	public static void main(String[] args) {
-//		yamadaは変数
 		Human01 yamada = new Human01(); //Human01クラスのインスタンスを生成
 		System.out.println("名前は" + yamada.name + "で、年齢は" + yamada.age + "です。");
 
 		Human01 sato = new Human01("佐藤", 25); //Human01クラスのインスタンスを生成
 		System.out.println("名前は" + sato.name + "で、年齢は" + sato.age + "です。");
 	}
-	
-//	Human01.javaの内容を引用してるって事やな。
-//	publicを使用して、どこからでもアクセスできるクラスHuman01を定義する。
-//	publicを用いてどこからでもアクセスできるメンバー変数nameとageを定義する。属性＝メンバー変数。
-	
 }
--- src/Human01.java
+++ src/Human01.java
@@ -1,19 +1,17 @@
 
 public class Human01 {
-	
-	// メンバー変数
-		public String name; 
-		public int age;  
+    // メンバー変数
+	public String name; 
+	public int age;  
 
-	    // 引数なしのコンストラクタ
-		public Human01() {
-			name = "山田";
-			age = 20;
-		}
-	    // 引数2つのコンストラクタ
-		public Human01(String name, int age) {
-			this.name = name;
-			this.age = age;
-		}
-
+    // 引数なしのコンストラクタ
+	public Human01() {
+		name = "山田";
+		age = 20;
+	}
+    // 引数2つのコンストラクタ
+	public Human01(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
 }
